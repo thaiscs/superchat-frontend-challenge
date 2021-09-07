3f5e9f697424c38f1d0ff58ad4e73b7486337356
thaiscs <thais.advj@gmail.com>
2021-09-07 18:24:13 +0200

    fixed typo on router link

--- find-fem-dev/components/ShareableLink.js
+++ find-fem-dev/components/ShareableLink.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 const ShareableLink = ({ username, reponame, topic }) => {
-  const baseURL = "wwww.find-female-devs.io/";
+  const baseURL = "www.find-female-devs.io/";
 
   return (
     <div className="mt-6">
